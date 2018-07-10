2682fe52801b42bdc6340d3faa86635a2e0a1d4d
xesam <xesamguo@gmail.com>
2018-07-10 23:57:13 +0800

    hello

--- src/main/java/io/github/xesam/jweb/HelloServlet.java
+++ src/main/java/io/github/xesam/jweb/HelloServlet.java
@@ -19,6 +19,6 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("hello servlet");
+        resp.getWriter().println(String.format("ServletName:%s,hello servlet", this.getServletName()));
     }
 }
--- webapp/index.jsp
+++ webapp/index.jsp
@@ -1,6 +1,7 @@
 <html>
 <body>
 <h2>Hello World!</h2>
-<p><a href="/greeting">greeting</a></p>
+<p><a href="/greeting1">greeting1</a></p>
+<p><a href="/greeting2">greeting2</a></p>
 </body>
 </html>
